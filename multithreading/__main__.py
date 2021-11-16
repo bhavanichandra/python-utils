3ff7c15f42a7b37b31a0f5dafcf7eaca5ea278df
bhavanichandra <bhavanichandra9@gmail.com>
2021-11-16 13:50:47 +0530

    Added new Logger factory class and removed unnecessary log_v1 decorator function

--- multithreading/__main__.py
+++ multithreading/__main__.py
@@ -1,15 +1,11 @@
-from logger.logger import log
-
 import logging
 import pathlib
+
+from logger.logger import LoggerFactory
 
 BASE_DIR = pathlib.Path(__file__).parent.resolve()
 config_file = BASE_DIR / 'config.yaml'
 
+LoggerFactory(config_file=config_file)
 
-@log(config_file=config_file.__str__())
-def test():
-    logging.info("test")
-
-
-test()
+logging.info('Starting multithreading')
